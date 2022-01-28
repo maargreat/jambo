f52ce0df4f3baff02b3f4e4d7d2bd189e0e6b20e
Maargreat <maargeat@gmail.com>
2022-01-28 16:03:59 +0300

    First Commit

---
+++ src/test/java/com/example/jambo/JamboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jambo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
